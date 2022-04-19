82f5ffefac95900569b729fb050420d4378f7b6f
Chernihina <annetchernigina@gmail.com>
2022-04-19 20:59:16 +0300

    Updates

---
+++ precache-manifest.b41330524f6495dc7b06edceb5bef73d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b1ef3786e7894c329928e452c98c59",
+    "url": "/reactMusicPlayer.github.io/index.html"
+  },
+  {
+    "revision": "ef79f1b166d4d20f728c",
+    "url": "/reactMusicPlayer.github.io/static/css/main.fb7fe8de.chunk.css"
+  },
+  {
+    "revision": "bb4f7772697348eb9fe5",
+    "url": "/reactMusicPlayer.github.io/static/js/2.0a3b4c19.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/reactMusicPlayer.github.io/static/js/2.0a3b4c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef79f1b166d4d20f728c",
+    "url": "/reactMusicPlayer.github.io/static/js/main.b1d4a932.chunk.js"
+  },
+  {
+    "revision": "9d7e5ae56169a06e02cd",
+    "url": "/reactMusicPlayer.github.io/static/js/runtime-main.42217960.js"
+  }
+]);
